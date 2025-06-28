3b99776efea0b127a031a4ae21d972e95bdb668e
DhruvRajpoot <dhruv.kutarya@gmail.com>
2025-06-29 04:27:57 +0530

    font sora import

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      'sora': ['Sora', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
