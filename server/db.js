72c4697e56e0696c613bc59af2c3990c0a2b88c8
DhruvRajpoot <dhruv.kutarya@gmail.com>
2025-06-28 07:35:09 +0530

    setup database

---
+++ server/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DB_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGODB_URL
+    : "mongodb://localhost:27017/intevuePoll";
+
+const connectdatabase = async () => {
+  try {
+    await mongoose.set("strictQuery", true);
+    await mongoose.connect(DB_URL);
+    console.log("connected to database successfully");
+  } catch (err) {
+    console.log("error while connecting to database ", err.message);
+  }
+};
+
+export default connectdatabase;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+import http from "http";
 import dotenv from "dotenv";
+import connectDatabase from "./db.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 8080;
+
+connectDatabase();
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
 });
